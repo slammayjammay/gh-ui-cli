85d281652c8f6502ac9d97f01335e92ee1a505c0
Scott McKenzie <slammayjammay@gmail.com>
2021-04-03 15:18:11 -0700

    Pad branch names

--- src/RepoUI/BranchesUI.js
+++ src/RepoUI/BranchesUI.js
@@ -1,5 +1,6 @@
 const fetcher = require('../fetcher');
 const vats = require('../vats');
+const pad = require('../pad');
 const ViStateUI = require('../ViStateUI');
 
 module.exports = class BranchesUI extends ViStateUI {
@@ -12,7 +13,7 @@ module.exports = class BranchesUI extends ViStateUI {
 		const url = this.repoData.branches_url.replace('{/branch}', '');
 		const branches = await (await fetcher.fetch(url)).json();
 
-		branches.forEach(({ name }) => this.div.addBlock(name));
+		branches.forEach(({ name }) => this.div.addBlock(pad(name, this.div.width())));
 
 		this.sync();
 		vats.emitEvent('state-change');
